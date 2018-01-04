7db8cc5f54bae96275b9dec94a51cb180f9a30a2
Geoff Cox <redgeoff@gmail.com>
2018-01-03 22:08:54 -0800

    feat(mac-os)

--- README.md
+++ README.md
@@ -2,13 +2,11 @@
 
 A VM for developing cordova apps on macOS
 
-
 ## Install Vagrant, VirtualBox and git
 
     http://www.vagrantup.com
     https://www.virtualbox.org (don't worry about setting up any VMs as the steps below will cover this)
     http://git-scm.com
-
 
 ## Set up
 
@@ -16,3 +14,13 @@ A VM for developing cordova apps on macOS
     $ cd cordova-vagrant
     $ vagrant up
     $ vagrant ssh
+    Launch macOS UI and open terminal window
+    Manually run mac-os.sh line by line
+
+## Create sample app:
+
+    $ cordova create hello com.example.hello HelloWorld
+    $ cd hello
+    $ cordova platform add ios
+    $ cordova emulate ios --target="iPhone-7, 11.2"
+
--- mac-os.sh
+++ mac-os.sh
@@ -1,1 +1,20 @@
 #!/usr/bin/env bash
+
+echo | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew doctor # do what it says
+brew tap caskroom/cask
+brew install node
+brew upgrade node
+brew cask install caskroom/versions/java8
+brew install ant maven gradle
+brew cask install android-sdk android-ndk
+sdkmanager "platform-tools" "platforms;android-26"
+sdkmanager --install "build-tools;26.0.2"
+echo "export ANDROID_HOME=/usr/local/share/android-sdk" >> ~/.bash_profile
+echo "export PATH=${PATH}:/usr/local/share/android-sdk/platform-tools:/usr/local/share/android-sdk/tools" >> ~/.bash_profile
+echo "export ANDROID_SDK_ROOT=/usr/local/share/android-sdk" >> ~/.bash_profile
+source ~/.bash_profile
+sudo npm install -g ios-deploy --unsafe-perm=true
+brew install CocoaPods
+npm install -g cordova
